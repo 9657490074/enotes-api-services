cd9961f87f3d3b30efa34b7c343c85067ecca474
Shashank Rawat <shashank.rawat8698@gmail.com>
2025-07-20 19:01:24 +0530

    reset-password

--- src/main/java/com/org/enotesapiservice/controller/HomeController.java
+++ src/main/java/com/org/enotesapiservice/controller/HomeController.java
@@ -1,15 +1,15 @@
 package com.org.enotesapiservice.controller;
 
+import com.org.enotesapiservice.dto.PasswordResetRequest;
 import com.org.enotesapiservice.exception.SuccessException;
 import com.org.enotesapiservice.service.HomeService;
+import com.org.enotesapiservice.service.UserService;
 import com.org.enotesapiservice.util.CommonUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     private final HomeService homeService;
+    private final UserService userService;
 
     @GetMapping("/verify")
     public ResponseEntity<?> verifyUserAccount(@RequestParam Integer uid,
@@ -27,5 +28,25 @@ public class HomeController {
         } else {
             return CommonUtil.createBuildResponseMessage("User account not verified", HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @GetMapping("/send-email-reset")
+    public ResponseEntity<?> sendEmailForPasswordReset(@RequestParam String email,
+                                                       HttpServletRequest request) throws Exception {
+        userService.sendEmailPasswordReset(email, request);
+        return CommonUtil.createBuildResponseMessage("email send success ! please reset password", HttpStatus.OK);
+    }
+
+    @GetMapping("/verify-password-link")
+    public ResponseEntity<?> verifyPasswordResetLink(@RequestParam Integer uid,
+                                                     @RequestParam String code) {
+        userService.verifyPasswordResetLink(uid,code);
+        return CommonUtil.createBuildResponseMessage("verified success",HttpStatus.OK);
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<?> resetPassword(@RequestBody PasswordResetRequest passwordResetRequest) {
+        userService.resetPassword(passwordResetRequest);
+        return CommonUtil.createBuildResponseMessage("password-reset-successfully",HttpStatus.OK);
     }
 }
--- src/main/java/com/org/enotesapiservice/service/HomeService.java
+++ src/main/java/com/org/enotesapiservice/service/HomeService.java
@@ -3,5 +3,6 @@ package com.org.enotesapiservice.service;
 import com.org.enotesapiservice.exception.SuccessException;
 
 public interface HomeService {
-    Boolean verifyUserAccount(Integer userId, String verificationCode) throws SuccessException;
+    Boolean verifyUserAccount(Integer userId,
+                              String verificationCode) throws SuccessException;
 }
